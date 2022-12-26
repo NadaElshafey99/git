86fc43a01c491e4e9304bf344770f475e0346ad3
HP <nadaelshafey99@gmail.com>
2022-12-26 14:34:39 +0200

    edit

--- NewCode.java
+++ NewCode.java
@@ -1,9 +1,16 @@
-public class NewCode {
+  public class NewCode {
 
     public NewCode() {}
 
 
+<<<<<<< Updated upstream
     protected void doIt() {
         System.out.println("DO IT!");
     }
 }  
+=======
+    protected void toDo() {
+        System.out.println("TO DO IT!");
+    }
+} 
+>>>>>>> Stashed changes
