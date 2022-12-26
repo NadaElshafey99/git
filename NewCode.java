d71376427c4dc21fa80b0fd7b664dcbc37525f5f
Moustafa Essam <drsh_201122@yahoo.com>
2022-12-26 16:05:47 +0200

    added do it

--- NewCode.java
+++ NewCode.java
@@ -1,16 +1,10 @@
-  public class NewCode {
+public class NewCode {
+
+    public void doIt() {
+ 
+    }
 
     public NewCode() {}
-
-
-<<<<<<< Updated upstream
-    protected void doIt() {
-        System.out.println("DO IT!");
-    }
-}  
-=======
-    protected void toDo() {
         System.out.println("TO DO IT!");
     }
 } 
->>>>>>> Stashed changes
