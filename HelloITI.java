2e46e99bddfdeed1dcfe38ba9232654899989736
HP <nadaelshafey99@gmail.com>
2022-12-26 12:46:42 +0200

    commit all Files

---
+++ HelloITI.java
@@ -0,0 +1,13 @@
+public class HelloITI
+{
+	public static void printHelloITI()
+	{
+		System.out.println("Hello ITI");
+		System.out.println("Hello ITI");
+
+	}
+	public static void main(String[] args)
+	{
+		printHelloITI();
+	}
+}
